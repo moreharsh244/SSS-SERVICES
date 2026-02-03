591e9e2485e4ce17c7922f44def38bd563a0ce6e
moreharsh244 <moreharsh244@gmail.com>
2026-02-03 22:49:46 +0530

    Update navbar brand link to point to products card page

--- admin/header.php
+++ admin/header.php
@@ -26,7 +26,7 @@ if (!isset($_SESSION['is_login'])) {
 <!-- Primary navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
   <div class="container-fluid">
-    <a class="navbar-brand fw-bold" href="view_product.php">Shree Swami Samarth</a>
+    <a class="navbar-brand fw-bold" href="products_card.php">Shree Swami Samarth</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
